3f1a0e3aabffd953a88d8785a8ed0f4a034e4b51
burgholzer <burgholzer@me.com>
2023-10-17 16:32:15 +0200

    ♻️ switch `CachedEdge` from `ComplexValue` to `std::complex`
    Signed-off-by: burgholzer <burgholzer@me.com>

--- include/dd/CachedEdge.hpp
+++ include/dd/CachedEdge.hpp
@@ -1,9 +1,10 @@
 #pragma once
 
-#include "dd/ComplexValue.hpp"
 #include "dd/DDDefinitions.hpp"
 
-#include <utility>
+#include <complex>
+#include <cstddef>
+#include <functional>
 
 namespace dd {
 
@@ -21,19 +22,17 @@ struct Complex;
  */
 template <typename Node> struct CachedEdge {
   Node* p{};
-  ComplexValue w{};
+  std::complex<fp> w{};
 
   CachedEdge() = default;
-  CachedEdge(Node* n, const ComplexValue& v) : p(n), w(v) {}
+  CachedEdge(Node* n, const std::complex<fp>& v) : p(n), w(v) {}
   CachedEdge(Node* n, const Complex& c);
 
   /// Comparing two DD edges with another involves comparing the respective
   /// pointers and checking whether the corresponding weights are "close enough"
   /// according to a given tolerance this notion of equivalence is chosen to
   /// counter floating point inaccuracies
-  bool operator==(const CachedEdge& other) const {
-    return p == other.p && w.approximatelyEquals(other.w);
-  }
+  bool operator==(const CachedEdge& other) const;
   bool operator!=(const CachedEdge& other) const { return !operator==(other); }
 };
 
@@ -41,6 +40,22 @@ template <typename Node> struct CachedEdge {
 
 namespace std {
 template <class Node> struct hash<dd::CachedEdge<Node>> {
+  /**
+   * @brief Compute the hash value for the given cached edge.
+   * @details The hash value is computed by combining the hash values of the
+   * node pointer and the weight. The hash value of the node pointer is computed
+   * using the murmur64 hash function on the pointer value. The hash value of
+   * the weight is computed by scaling the real and imaginary part by the
+   * tolerance of the real numbers, rounding the result to the nearest
+   * integer and computing the hash value of the resulting pair of integers.
+   * @param e The cached edge to compute the hash value for.
+   * @returns The hash value for the given cached edge.
+   * @note It is rather hard to define good hash functions for floating point
+   * numbers. This hash function is not perfect, but it is fast and should
+   * provide a good distribution of hash values. Furthermore, two floating point
+   * numbers that are within the tolerance of the complex table will always
+   * produce the same hash value.
+   */
   std::size_t operator()(dd::CachedEdge<Node> const& e) const noexcept;
 };
 } // namespace std
--- src/dd/CachedEdge.cpp
+++ src/dd/CachedEdge.cpp
@@ -4,12 +4,17 @@
 #include "dd/Node.hpp"
 #include "dd/RealNumber.hpp"
 
+#include <complex>
+
 namespace dd {
 
 template <typename Node>
-CachedEdge<Node>::CachedEdge(Node* n, const Complex& c) : p(n) {
-  w.r = RealNumber::val(c.r);
-  w.i = RealNumber::val(c.i);
+CachedEdge<Node>::CachedEdge(Node* n, const Complex& c)
+    : p(n), w(static_cast<std::complex<fp>>(c)) {}
+
+template <typename Node>
+bool CachedEdge<Node>::operator==(const CachedEdge& other) const {
+  return p == other.p && std::abs(w - other.w) < RealNumber::eps;
 }
 
 template struct CachedEdge<vNode>;
@@ -23,8 +28,11 @@ template <class Node>
 std::size_t hash<dd::CachedEdge<Node>>::operator()(
     const dd::CachedEdge<Node>& e) const noexcept {
   const auto h1 = dd::murmur64(reinterpret_cast<std::size_t>(e.p));
-  const auto h2 = std::hash<dd::ComplexValue>{}(e.w);
-  return dd::combineHash(h1, h2);
+  const auto h2 = dd::murmur64(
+      static_cast<std::size_t>(std::round(e.w.real() / dd::RealNumber::eps)));
+  const auto h3 = dd::murmur64(
+      static_cast<std::size_t>(std::round(e.w.imag() / dd::RealNumber::eps)));
+  return dd::combineHash(h1, dd::combineHash(h2, h3));
 }
 
 template struct hash<dd::CachedEdge<dd::vNode>>;
